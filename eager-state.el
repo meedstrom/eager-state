1151587ce500e015e8f5d8a19a885e9281c868b8
Martin Edström <meedstrom@runbox.eu>
2025-12-18 08:21:11 +0100

    BREAKING: Deep refactor for 0.2.0

--- eager-state.el
+++ eager-state.el
@@ -1,179 +1,250 @@
-;;; eager-state.el --- Eagerly cache recentf and other data -*- lexical-binding: t -*-
+;;; eager-state.el --- Pre-empt kill-emacs-hook with a crash-only design  -*- lexical-binding: t -*-
 
-;; Author: Martin Edström <meedstrom91@gmail.com>
-;; Created: 2024-03-24
-;; Version: 0.1
-;; Keywords: tools
-;; Homepage: https://github.com/meedstrom/eager-state
-;; Package-Requires: ((emacs "25.1"))
+;; Copyright (C) 2024-2025 Martin Edström
 
-;; Copyright (C) 2024 Martin Edström
-;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-;; This file is not part of GNU Emacs.
+;; Author: Martin Edström <meedstrom91@gmail.com>
+;; Created: 2024-03-24
+;; Version: 0.2.0
+;; Keywords: convenience
+;; Homepage: https://github.com/meedstrom/eager-state
+;; Package-Requires: ((emacs "29.1") (llama "0.5.0"))
 
 ;;; Commentary:
 
-;; Ensure survival of data such as recentf, that normally rely on
+;; For justification, please see file README.org, or equivalently,
+;; Info node "(eager-state)".
+
+;; An example setup looks like:
+
+;;     (require 'eager-state)
+;;     (setq eager-state-kill-emacs-hook-subset
+;;           '(bookmark-exit-hook-internal
+;;             savehist-autosave
+;;             transient-maybe-save-history
+;;             org-clock-save
+;;             org-id-locations-save
+;;             save-place-kill-emacs-hook
+;;             recentf-save-list
+;;             recentf-cleanup
+;;             doom-cleanup-project-cache-h
+;;             doom-persist-scratch-buffers-h))
+
+;; Note how the above list is cribbed from real-world values of
 ;; `kill-emacs-hook'.
-;;
-;; It's sloppy design to put data-syncs on `kill-emacs-hook'.  Most of
-;; the time my Emacs goes down, it's a crash, SIGTERM, kernel panic,
-;; system freeze, segfault, yanked power cord, accidental power button
-;; event, or drained battery.  I'm not sure who is the mythical user
-;; who regularly types C-x C-c to bring down a fully functional Emacs.
-;;
-;; I'm missing some data every time I start Emacs: I can't find org
-;; notes by org-id, recentf suffers partial amnesia, and so on.
-;;
-;; This mode fixes all that by preemptively running most of
-;; `kill-emacs-hook' while the user is idle.
-;;
-;; --------------------------------------------------------------
-;; End-user setup:
-;;
-;; (require 'eager-state)
-;; (eager-state-preempt-kill-emacs-hook-mode)
-;;
-;; You may also configure `eager-state-preempt-functions': add members
-;; from your own `kill-emacs-hook' that look like they would belong.
-;;
-;; As an optional bonus, knowing that the data is kept well-synced
-;; allows you to kill/restart Emacs much faster:
-;;
-;; (advice-add #'kill-emacs :before (lambda (&rest _) (setq kill-emacs-hook nil)))
+
+;; The `require' statement starts a timer that periodically tries to run
+;; the above listed functions.
+
+;; You can add any symbol to the list, even if it is not yet defined as a
+;; function.  A given symbol will only be funcalled if it is present both in
+;; that list, and in `kill-emacs-hook'.
+
+;; To add new functions that you don't want on `kill-emacs-hook', add
+;; them to `eager-state-sync-hook' instead.
 
 ;;; Code:
 
+(require 'cl-lib)
+(require 'llama)
+(require 'benchmark)
+
 (defgroup eager-state nil
-  "Eagerly cache recentf and other data."
-  :group 'development)
+  "Eagerly persist data onto disk."
+  :group 'convenience
+  :link '(url-link :tag "GitHub source" "https://github.com/meedstrom/eager-state")
+  :link '(info-link :tag "Info manual" "(eager-state)"))
+
+(defcustom eager-state-inhibit nil
+  "If t, do not auto-enable `eager-state-mode' at load time.
+Must be set before loading eager-state.el."
+  :type 'boolean)
+
+(defcustom eager-state-kill-emacs-hook-subset nil
+  "Subset of `kill-emacs-hook' to pre-empt.
+These symbols need not be known to be defined,
+and are only funcalled if also present in `kill-emacs-hook'."
+  :type 'hook
+  :options '(bookmark-exit-hook-internal
+             doom-cleanup-project-cache-h
+             doom-persist-scratch-buffers-h
+             org-babel-remove-temporary-directory
+             org-babel-remove-temporary-stable-directory
+             org-clock-save
+             org-id-locations-save
+             org-persist-clear-storage-maybe
+             org-persist-gc
+             org-persist-write-all
+             recentf-cleanup
+             recentf-save-list
+             save-place-kill-emacs-hook
+             savehist-autosave
+             tramp-dump-connection-properties
+             transient-maybe-save-history))
+
+(defcustom eager-state-kill-emacs-query-functions-subset nil
+  "Subset of `kill-emacs-query-functions' to pre-empt.
+These symbols need not be known to be defined,
+and are only funcalled if also present in `kill-emacs-query-functions'."
+  :type 'hook
+  :options '(desktop-kill
+             eshell-save-some-history
+             eshell-save-some-last-dir))
+
+(defcustom eager-state-faster-shutdown nil
+  "If t, do not run any function in the subset lists at Emacs shutdown.
+
+In other words, all members of
+- `eager-state-kill-emacs-query-functions-subset' and
+- `eager-state-kill-emacs-hook-subset'
+are removed from the corresponding hooks before those hooks run.
+
+Also removed from `kill-emacs-hook' are all functions that happen to be
+members of `eager-state-sync-hook'."
+  :type 'boolean)
 
 (defcustom eager-state-sync-hook nil
-  "Functions to run after 3 idle minutes, or at shutdown."
-  :type '(repeat sexp))
+  "Functions called intermittently to sync data to disk."
+  :type 'hook)
 
-(defcustom eager-state-enable-hook nil
-  "Functions to run when enabling `eager-state-mode'."
-  :type '(repeat sexp))
+(defcustom eager-state-idle-delay 15
+  "Be idle at least this long before actually syncing.
+An idle timer of this duration starts every `eager-state-periodic-delay'.
 
-(defcustom eager-state-disable-hook nil
-  "Functions to run when disabling `eager-state-mode'."
-  :type '(repeat sexp))
+If this value exceeds `eager-state-periodic-delay', Eager-State will
+only use a repeating idle timer, and no periodic timer at all."
+  :type 'number)
 
-;; Suggestions welcome!  We want this list as big as possible.
-(defcustom eager-state-preempt-functions
-  '(bookmark-exit-hook-internal
-    savehist-autosave
-    transient-maybe-save-history
-    org-roam-db--close-all
-    org-clock-save
-    org-id-locations-save
-    org-persist-gc
-    org-persist-write-all
-    org-persist-clear-storage-maybe
-    org-babel-remove-temporary-stable-directory
-    org-babel-remove-temporary-directory
-    save-place-kill-emacs-hook
-    recentf-save-list
-    recentf-cleanup
-    doom-cleanup-project-cache-h
-    doom-persist-scratch-buffers-h)
-  "The subset of `kill-emacs-hook' you want to run regularly.
-For `eager-state-preempt-kill-emacs-hook-mode' to call a function
-regularly, that function must be in both this list and in
-`kill-emacs-hook'.  It is fine to add functions that have not yet
-been defined.
+(defcustom eager-state-periodic-delay 60
+  "Minimum delay between attempts to sync, in seconds.
+Set to 0 if you want to only use `eager-state-idle-delay'."
+  :type 'number)
 
-The net effect is that you can fill this list with functions from
-packages you no longer use, and they will simply be ignored, plus
-that even for the packages that do exist -- let's say org -- will
-not do their sync business until you have loaded org, because
-that's when org adds its business to `kill-emacs-hook'."
-  :type '(repeat sexp))
+(defvar eager-state--periodic-timer (timer-create))
+(defvar eager-state--idle-timer (timer-create))
+(defun eager-state--check ()
+  "Adjust timers and maybe call `eager-state--sync'."
+  (let ((idle (float-time (or (current-idle-time) 0))))
+    (if (>= eager-state-idle-delay
+            eager-state-periodic-delay)
+        ;; If user set `eager-state-idle-delay' higher than
+        ;; `eager-state-periodic-delay', switch to using idle timer only.
+        (progn
+          (cancel-timer eager-state--periodic-timer)
+          (if (and (member eager-state--idle-timer timer-idle-list)
+                   (eq (round (timer--time eager-state--idle-timer))
+                       (round eager-state-idle-delay)))
+              (when (> idle eager-state-idle-delay)
+                (eager-state--sync))
+            (cancel-timer eager-state--idle-timer)
+            (setq eager-state--idle-timer
+                  (run-with-idle-timer eager-state-idle-delay
+                                       t
+                                       #'eager-state--check))))
+      (cancel-timer eager-state--idle-timer)
+      (if (and (member eager-state--periodic-timer timer-list)
+               (eq (round (timer--repeat-delay eager-state--periodic-timer))
+                   (round eager-state-periodic-delay)))
+          (if (> idle eager-state-periodic-delay)
+              ;; Don't repeatedly sync during a long idle
+              nil
+            (setq eager-state--idle-timer
+                  (run-with-idle-timer eager-state-idle-delay
+                                       nil
+                                       #'eager-state--sync)))
+        (cancel-timer eager-state--periodic-timer)
+        (setq eager-state--periodic-timer
+              (run-with-timer eager-state-periodic-delay
+                              eager-state-periodic-delay
+                              #'eager-state--check))))))
 
-(defvar eager-state--kill-emacs-hook-subset nil
-  "Internal variable, do not set!
-Dynamic (global) variable holding hooks to run, used by
-`eager-state-update'.  Naively, that function could just let-bind
-a list of hooks internally, but `run-hooks' will not work on
-lexically scoped variables.")
+(defun eager-state--sync ()
+  "Run relevant hooks silently, and profile time elapsed for each one."
+  (let ((functions
+         (append (seq-intersection kill-emacs-query-functions
+                                   eager-state-kill-emacs-query-functions-subset)
+                 (seq-intersection kill-emacs-hook
+                                   eager-state-kill-emacs-hook-subset)
+                 eager-state-sync-hook))
+        (save-silently t)
+        (inhibit-message t)
+        (standard-output #'ignore)
+        (buf (get-buffer-create " *eager-state*")))
+    (message "eager-state: Syncing...")
+    (cl-letf (((symbol-function 'write-region)
+               (let ((real-write-region (symbol-function 'write-region)))
+                 (##apply real-write-region %1 %2 %3 &4 (or &5 'quiet) &*))))
+      (with-current-buffer buf
+        (goto-char (point-max))
+        (insert (format-time-string "\n%F %T: Starting...\n"))
+        (dolist (func functions)
+          (insert (if (symbolp func) (symbol-name func) "anonymous lambda")
+                  "...")
+          (let ((elapsed (benchmark-elapse (funcall func))))
+            (cl-assert (eq buf (current-buffer)))
+            (insert (format " took %.2fs" elapsed)
+                    "\n")))))
+    (message "eager-state: Syncing...done")))
 
-(defun eager-state-update ()
-  "Write histories and caches to disk."
-  (run-hooks 'eager-state-sync-hook)
-  (when eager-state-preempt-kill-emacs-hook-mode
-    (setq eager-state--kill-emacs-hook-subset
-          (seq-intersection kill-emacs-hook eager-state-preempt-functions))
-    (run-hooks 'eager-state--kill-emacs-hook-subset)))
+(defun eager-state--trim-kill-emacs-hook (&rest _)
+  "Maybe delete members of `kill-emacs-hook'."
+  (when eager-state-faster-shutdown
+    (dolist (func (append eager-state-sync-hook
+                          eager-state-kill-emacs-hook-subset))
+      (setq kill-emacs-hook (delete func kill-emacs-hook)))))
 
-(defun eager-state-kill-emacs-hook ()
-  "Variant of `eager-state-update'.
-Since this variant is meant to run on `kill-emacs-hook', it will
-not run `eager-state-preempt-functions', as that would cause many
-functions to run twice."
-  (run-hooks 'eager-state-sync-hook))
+(defun eager-state--trim-kill-emacs-query-functions (&rest _)
+  "Maybe delete members of `kill-emacs-query-functions'."
+  (when eager-state-faster-shutdown
+    (dolist (func eager-state-kill-emacs-query-functions-subset)
+      (setq kill-emacs-query-functions (delete func kill-emacs-query-functions)))))
 
 ;;;###autoload
-(define-minor-mode eager-state-preempt-kill-emacs-hook-mode
-  "Start running most of `kill-emacs-hook' regularly."
-  :global t
-  (if eager-state-preempt-kill-emacs-hook-mode
-      (when (not eager-state-mode)
-        (eager-state-preempt-kill-emacs-hook-mode 0)
-        (message "%s" "`eager-state-mode' not enabled, so not enabling `eager-state-preempt-kill-emacs-hook-mode'"))))
-
-(defvar eager-state-timer (timer-create)
-  "The timer used by `eager-state-mode'.")
-
 (define-minor-mode eager-state-mode
-  "Regularly write caches to disk."
-  :global t
+  "Regularly write various kinds of persistable cache to disk.
+
+Users can add to `eager-state-kill-emacs-hook-subset' any members of
+`kill-emacs-hook' that they want to run preemptively every now and then.
+Typical members relate to Save-Place, Recentf and similar.
+
+Package developers can add to `eager-state-sync-hook' what they would
+normally add to `kill-emacs-hook'.  That avoids needing to define new
+timers and timer intervals, unifying all that under the user options
+`eager-state-periodic-delay' and `eager-state-idle-delay'.
+
+Package developers should not enable this mode.  Simply `require'
+the library, and it is turned on by default unless the user changes
+that, which should be respected.
+
+-----"
   :require 'eager-state
-  (cancel-timer eager-state-timer)
+  :global t
   (if eager-state-mode
       (progn
-        (setq eager-state-timer
-              (run-with-idle-timer (* 3 60) t #'eager-state-update))
-        (add-hook 'kill-emacs-hook #'eager-state-kill-emacs-hook)
-        (run-hooks 'eager-state-enable-hook))
-    (remove-hook 'kill-emacs-hook #'eager-state-kill-emacs-hook)
-    (run-hooks 'eager-state-disable-hook)))
+        (advice-add #'kill-emacs :before #'eager-state--trim-kill-emacs-hook)
+        (advice-add #'save-buffers-kill-emacs :before #'eager-state--trim-kill-emacs-query-functions)
+        (cancel-timer eager-state--periodic-timer)
+        (cancel-timer eager-state--idle-timer)
+        (eager-state--check))
+    (advice-remove #'kill-emacs #'eager-state--trim-kill-emacs-hook)
+    (advice-remove #'save-buffers-kill-emacs #'eager-state--trim-kill-emacs-query-functions)
+    (cancel-timer eager-state--periodic-timer)
+    (cancel-timer eager-state--idle-timer)))
 
-(defvar eager-state-prevent nil
-  "Prevent auto-enabling `eager-state-mode' at load time.")
-
-;; NOTE: It's a code-smell to enable a mode right upon loading the file,
-;; but necessary because it's meant as a library for other packages, none
-;; of which should have to check that the mode is on.  They should be able
-;; to just write:
-;;
-;;    (require 'eager-state)
-;;    (add-hook 'eager-state-sync-hook #'your-persist-function)
-;;
-;; and be done with it.  Plus, if the end-user turned `eager-state-mode' off,
-;; that choice should be respected, so it would be inappropriate to expect
-;; every package to turn it on for us.
-;;
-;; Anyway, since we have a nil default value for `eager-state-sync-hook',
-;; the mode actually does nothing until some other mode you enabled adds
-;; something there.
-(unless eager-state-prevent
-  (eager-state-mode)
-  ;; Run only the first time the file is loaded
-  (setq eager-state-prevent t))
+(unless eager-state-inhibit
+  (eager-state-mode))
 
 (provide 'eager-state)
 ;;; eager-state.el ends here
