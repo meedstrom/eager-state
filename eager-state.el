45ccddb9993c99c722c64a0eff4d9a6b48d8b475
Martin Edström <meedstrom@runbox.eu>
2025-12-18 10:37:35 +0100

    Lint

--- eager-state.el
+++ eager-state.el
@@ -1,4 +1,4 @@
-;;; eager-state.el --- Pre-empt kill-emacs-hook with a crash-only design  -*- lexical-binding: t -*-
+;;; eager-state.el --- Eagerly persist data onto disk  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2024-2025 Martin Edström
 
@@ -24,12 +24,16 @@
 
 ;;; Commentary:
 
-;; For justification, please see file README.org, or equivalently,
-;; Info node "(eager-state)".
+;; Provide a simple way to periodically run a subset of `kill-emacs-hook',
+;; ensuring that state variables are written to disk long before something
+;; happens to your Emacs session.
 
 ;; An example setup looks like:
 
 ;;     (require 'eager-state)
+;;     (setq eager-state-idle-delay 15)
+;;     (setq eager-state-periodic-delay 60)
+;;     (setq eager-state-faster-shutdown t)
 ;;     (setq eager-state-kill-emacs-hook-subset
 ;;           '( bookmark-exit-hook-internal
 ;;              savehist-autosave
@@ -45,8 +49,9 @@
 ;; Note how the above list is cribbed from real-world values of
 ;; `kill-emacs-hook'.
 
-;; The `require' statement starts a timer that periodically tries to run
-;; the above listed functions.
+;; The `require' statement enables a global minor mode `eager-state-mode',
+;; which manages a timer that periodically tries to run the above listed
+;; functions.
 
 ;; You can add any symbol to the list, even if it is not yet defined as a
 ;; function.  A given symbol will only be funcalled if it is present both in
